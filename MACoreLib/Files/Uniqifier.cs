4a79596be37c0eb7c0d3ac7b0f34b403cfd97c5e
Hiromitsu Oshiba <h.oshiba.0117@gmail.com>
2017-06-03 20:49:17 +0900

    ADD: file uniqifier

---
+++ MACoreLib/Files/Uniqifier.cs
@@ -0,0 +1,80 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+using MACoreLib.Exceptions;
+
+namespace MACoreLib.Files
+{
+
+	/// <summary>Exception thrown when enumeration failed.</summary>
+	public class FileEnumerationFailedException : MACoreException { };
+
+	/// <summary>Uniqify files in directory</summary>
+	public class Uniqifier
+	{
+		private string root;
+
+		/// <summary></summary>
+		/// <exception cref="System.IO.DirectoryNotFoundException">
+		///   <paramref name="dirroot">throwed when dirroot not found.</paramref>
+		/// </exception>
+		public Uniqifier(string dirroot)
+		{
+			if (Directory.Exists(dirroot) == false)
+			{
+				throw new DirectoryNotFoundException(dirroot + " not founds.");
+			}
+			this.root = dirroot;
+		}
+
+		/// <summary>get unique files. files uniqued by sha256</summary>
+		/// <returns>iterator of string</returns>
+		/// <exception cref="MACoreLib.Files.FileEnumerationFailedException"></exception>
+		public IEnumerable<string> GetUniquedFiles()
+		{
+
+			string[] files;
+
+			var alreadySeen = new Dictionary<string, bool>();
+
+			try
+			{
+				// listup all files in directory
+				files = Directory.GetFiles(this.root, "*", SearchOption.AllDirectories);
+			}
+			catch
+			{
+				throw new FileEnumerationFailedException();
+			}
+
+			foreach (var f in files)
+			{
+				string sha256 = "";
+
+				try
+				{
+					// calc sha256
+					var filehash = new FileHash(f);
+					sha256 = filehash.GetSha256Sum();
+				}
+				catch
+				{
+					// skip when calcuration failed.
+					continue;
+				}
+
+				if (alreadySeen.ContainsKey(sha256))
+				{
+					// skip already picked.
+					continue;
+				}
+
+				yield return f;
+				alreadySeen[sha256] = true;
+			}
+		}
+
+	}
+
+}
---
+++ MACoreLibTest/Files/UniqifierTest.cs
@@ -0,0 +1,26 @@
+﻿using MACoreLib.Files;
+using System;
+using System.IO;
+using Xunit;
+
+namespace MACoreLibTest.Files
+{
+    public class UniqifierTest
+    {
+        [Fact]
+        public void TestUniqify()
+        {
+            var d = Path.GetTempPath();
+
+            var uniq = new Uniqifier(d);
+
+            foreach (var f in uniq.GetUniquedFiles())
+            {
+                Console.WriteLine(f);
+            }
+
+            Assert.True(true);
+
+        }
+    }
+}
